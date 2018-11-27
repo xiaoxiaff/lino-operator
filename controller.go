9e7dc47660173971b31b0f56e0da42a0b4dace64
Yukai Tu <ytu@lino.network>
2018-11-27 11:50:52 -0800

    dep ensure

---
+++ controller.go
@@ -0,0 +1,259 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/golang/glog"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/util/runtime"
+	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
+	"k8s.io/apimachinery/pkg/util/wait"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/kubernetes/scheme"
+	typedcorev1 "k8s.io/client-go/kubernetes/typed/core/v1"
+	"k8s.io/client-go/tools/cache"
+	"k8s.io/client-go/tools/record"
+	"k8s.io/client-go/util/workqueue"
+
+	listers "github.com/lino-network/lino-operator/pkg/client/listers/samplecrd/v1"
+	samplecrdv1 "github.com/resouer/k8s-controller-custom-resource/pkg/apis/samplecrd/v1"
+	clientset "github.com/resouer/k8s-controller-custom-resource/pkg/client/clientset/versioned"
+	informers "github.com/resouer/k8s-controller-custom-resource/pkg/client/informers/externalversions/samplecrd/v1"
+)
+
+const controllerAgentName = "fullnode-controller"
+
+const (
+	// SuccessSynced is used as part of the Event 'reason' when a Network is synced
+	SuccessSynced = "Synced"
+
+	// MessageResourceSynced is the message used for an Event fired when a Fullnode
+	// is synced successfully
+	MessageResourceSynced = "Fullnode synced successfully"
+)
+
+// Controller is the controller implementation for Network resources
+type Controller struct {
+	// kubeclientset is a standard kubernetes clientset
+	kubeclientset kubernetes.Interface
+	// fullnodeclientset is a clientset for our own API group
+	fullnodeclientset clientset.Interface
+
+	fullnodeLister  listers.FullnodeLister
+	fullnodesSynced cache.InformerSynced
+
+	// workqueue is a rate limited work queue. This is used to queue work to be
+	// processed instead of performing it as soon as a change happens. This
+	// means we can ensure we only process a fixed amount of resources at a
+	// time, and makes it easy to ensure we are never processing the same item
+	// simultaneously in two different workers.
+	workqueue workqueue.RateLimitingInterface
+	// recorder is an event recorder for recording Event resources to the
+	// Kubernetes API.
+	recorder record.EventRecorder
+}
+
+// NewController returns a new fullnode controller
+func NewController(
+	kubeclientset kubernetes.Interface,
+	fullnodeclientset clientset.Interface,
+	fullnodeInformer informers.FullnodeInformer) *Controller {
+
+	// Create event broadcaster
+	// Add sample-controller types to the default Kubernetes Scheme so Events can be
+	// logged for sample-controller types.
+	utilruntime.Must(fullnodescheme.AddToScheme(scheme.Scheme))
+	glog.V(4).Info("Creating event broadcaster")
+	eventBroadcaster := record.NewBroadcaster()
+	eventBroadcaster.StartLogging(glog.Infof)
+	eventBroadcaster.StartRecordingToSink(&typedcorev1.EventSinkImpl{Interface: kubeclientset.CoreV1().Events("")})
+	recorder := eventBroadcaster.NewRecorder(scheme.Scheme, corev1.EventSource{Component: controllerAgentName})
+
+	controller := &Controller{
+		kubeclientset:     kubeclientset,
+		fullnodeclientset: fullnodeclientset,
+		fullnodesLister:   fullnodeInformer.Lister(),
+		fullnodesSynced:   fullnodeInformer.Informer().HasSynced,
+		workqueue:         workqueue.NewNamedRateLimitingQueue(workqueue.DefaultControllerRateLimiter(), "Fullnodes"),
+		recorder:          recorder,
+	}
+
+	glog.Info("Setting up event handlers")
+	// Set up an event handler for when Fullnode resources change
+	fullnodeInformer.Informer().AddEventHandler(cache.ResourceEventHandlerFuncs{
+		AddFunc: controller.enqueueFullnode,
+		UpdateFunc: func(old, new interface{}) {
+			oldFullnode := old.(*samplecrdv1.Fullnode)
+			newFullnode := new.(*samplecrdv1.Fullnode)
+			if oldFullnode.ResourceVersion == newFullnode.ResourceVersion {
+				// Periodic resync will send update events for all known Fullnodes.
+				// Two different versions of the same Fullnode will always have different RVs.
+				return
+			}
+			controller.enqueueFullnode(new)
+		},
+		DeleteFunc: controller.enqueueFullnodeForDelete,
+	})
+
+	return controller
+}
+
+// Run will set up the event handlers for types we are interested in, as well
+// as syncing informer caches and starting workers. It will block until stopCh
+// is closed, at which point it will shutdown the workqueue and wait for
+// workers to finish processing their current work items.
+func (c *Controller) Run(threadiness int, stopCh <-chan struct{}) error {
+	defer runtime.HandleCrash()
+	defer c.workqueue.ShutDown()
+
+	// Start the informer factories to begin populating the informer caches
+	glog.Info("Starting Network control loop")
+
+	// Wait for the caches to be synced before starting workers
+	glog.Info("Waiting for informer caches to sync")
+	if ok := cache.WaitForCacheSync(stopCh, c.networksSynced); !ok {
+		return fmt.Errorf("failed to wait for caches to sync")
+	}
+
+	glog.Info("Starting workers")
+	// Launch two workers to process Network resources
+	for i := 0; i < threadiness; i++ {
+		go wait.Until(c.runWorker, time.Second, stopCh)
+	}
+
+	glog.Info("Started workers")
+	<-stopCh
+	glog.Info("Shutting down workers")
+
+	return nil
+}
+
+// runWorker is a long-running function that will continually call the
+// processNextWorkItem function in order to read and process a message on the
+// workqueue.
+func (c *Controller) runWorker() {
+	for c.processNextWorkItem() {
+	}
+}
+
+// processNextWorkItem will read a single work item off the workqueue and
+// attempt to process it, by calling the syncHandler.
+func (c *Controller) processNextWorkItem() bool {
+	obj, shutdown := c.workqueue.Get()
+
+	if shutdown {
+		return false
+	}
+
+	// We wrap this block in a func so we can defer c.workqueue.Done.
+	err := func(obj interface{}) error {
+		// We call Done here so the workqueue knows we have finished
+		// processing this item. We also must remember to call Forget if we
+		// do not want this work item being re-queued. For example, we do
+		// not call Forget if a transient error occurs, instead the item is
+		// put back on the workqueue and attempted again after a back-off
+		// period.
+		defer c.workqueue.Done(obj)
+		var key string
+		var ok bool
+		// We expect strings to come off the workqueue. These are of the
+		// form namespace/name. We do this as the delayed nature of the
+		// workqueue means the items in the informer cache may actually be
+		// more up to date that when the item was initially put onto the
+		// workqueue.
+		if key, ok = obj.(string); !ok {
+			// As the item in the workqueue is actually invalid, we call
+			// Forget here else we'd go into a loop of attempting to
+			// process a work item that is invalid.
+			c.workqueue.Forget(obj)
+			runtime.HandleError(fmt.Errorf("expected string in workqueue but got %#v", obj))
+			return nil
+		}
+		// Run the syncHandler, passing it the namespace/name string of the
+		// Network resource to be synced.
+		if err := c.syncHandler(key); err != nil {
+			return fmt.Errorf("error syncing '%s': %s", key, err.Error())
+		}
+		// Finally, if no error occurs we Forget this item so it does not
+		// get queued again until another change happens.
+		c.workqueue.Forget(obj)
+		glog.Infof("Successfully synced '%s'", key)
+		return nil
+	}(obj)
+
+	if err != nil {
+		runtime.HandleError(err)
+		return true
+	}
+
+	return true
+}
+
+// syncHandler compares the actual state with the desired, and attempts to
+// converge the two. It then updates the Status block of the Fullnode resource
+// with the current status of the resource.
+func (c *Controller) syncHandler(key string) error {
+	// Convert the namespace/name string into a distinct namespace and name
+	namespace, name, err := cache.SplitMetaNamespaceKey(key)
+	if err != nil {
+		runtime.HandleError(fmt.Errorf("invalid resource key: %s", key))
+		return nil
+	}
+
+	// Get the Fullnode resource with this namespace/name
+	fullnode, err := c.fullnodeLister.Fullnodes(namespace).Get(name)
+	if err != nil {
+		// The Fullnode resource may no longer exist, in which case we stop
+		// processing.
+		if errors.IsNotFound(err) {
+			glog.Warningf("Fullnode: %s/%s does not exist in local cache, will delete it ...",
+				namespace, name)
+
+			glog.Infof(" Deleting fullnode: %s/%s ...", namespace, name)
+
+			// FIX ME: call Neutron API to delete this network by name.
+			//
+			// neutron.Delete(namespace, name)
+
+			return nil
+		}
+
+		runtime.HandleError(fmt.Errorf("failed to list fullnode by: %s/%s", namespace, name))
+
+		return err
+	}
+
+	glog.Infof("Try to process fullnode: %#v ...", fullnode)
+
+	c.recorder.Event(fullnode, corev1.EventTypeNormal, SuccessSynced, MessageResourceSynced)
+	return nil
+}
+
+// enqueueFullnode takes a Fullnode resource and converts it into a namespace/name
+// string which is then put onto the work queue. This method should *not* be
+// passed resources of any type other than Fullnode.
+func (c *Controller) enqueueFullnode(obj interface{}) {
+	var key string
+	var err error
+	if key, err = cache.MetaNamespaceKeyFunc(obj); err != nil {
+		runtime.HandleError(err)
+		return
+	}
+	c.workqueue.AddRateLimited(key)
+}
+
+// enqueueFullnodeForDelete takes a deleted Fullnode resource and converts it into a namespace/name
+// string which is then put onto the work queue. This method should *not* be
+// passed resources of any type other than Fullnode.
+func (c *Controller) enqueueFullnodeForDelete(obj interface{}) {
+	var key string
+	var err error
+	key, err = cache.DeletionHandlingMetaNamespaceKeyFunc(obj)
+	if err != nil {
+		runtime.HandleError(err)
+		return
+	}
+	c.workqueue.AddRateLimited(key)
+}
